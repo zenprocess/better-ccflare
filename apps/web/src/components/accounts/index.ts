11942e22b1d1e87b981424467516d079b5e26495
ao-reconcile <ao-reconcile@zp.digital>
2026-08-04 14:42:30 +0300

    WIP auto-salvage: reconciling worktree for terminated session ccflare-89

--- apps/web/src/components/accounts/index.ts
+++
@@ -1,6 +0,0 @@
-export { AccountAddForm } from "./AccountAddForm";
-export { AccountList } from "./AccountList";
-export { AccountListItem } from "./AccountListItem";
-export { DeleteConfirmationDialog } from "./DeleteConfirmationDialog";
-export { RateLimitProgress } from "./RateLimitProgress";
-export { RenameAccountDialog } from "./RenameAccountDialog";
