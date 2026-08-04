9f0495355d15c857540e8fec365b7a4f1cac53d1
ao-reconcile <ao-reconcile@zp.digital>
2026-08-04 14:42:32 +0300

    WIP auto-salvage: reconciling worktree for terminated session ccflare-92

--- packages/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Export router - the main public API
-
-export { stopAllOAuthCallbackForwarders } from "./handlers/oauth";
-export { APIRouter } from "./router";
-
-// Export types
-export * from "./types";
