a8e212e5175170172ee5f964551841f4e8295f1f
ao-reconcile <ao-reconcile@zp.digital>
2026-08-04 14:42:27 +0300

    WIP auto-salvage: reconciling worktree for terminated session ccflare-83

--- packages/core/src/di.ts
+++
@@ -1,9 +0,0 @@
-export { container } from "./container";
-
-export const SERVICE_KEYS = {
-	Logger: Symbol("Logger"),
-	Config: Symbol("Config"),
-	Database: Symbol("Database"),
-	PricingLogger: Symbol("PricingLogger"),
-	AsyncWriter: Symbol("AsyncWriter"),
-} as const;
